85e241595e6a5cde4eb1eeceab9bd02cbc6de549
Himanshu Tamoli <himanshutamoli2005@gmail.com>
2024-10-02 00:02:02 +0530

    Your message describing the changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import Map from './component/Map/Map'
 import axios from 'axios';
 
 function App() {
- const [loc,setLoc] = useState(null)
+  const [loc, setLoc] = useState(null)
   useEffect(() => {
     const fetchIPInfo = async () => {
       try {
         const response = await axios.get(`https://ipinfo.io?token=42456662029b4f`);
-        const data =  response.data
+        const data = response.data
         setLoc(data.loc.split(','));
       } catch (error) {
         console.error('Error fetching IP info:', error);
@@ -18,16 +18,16 @@ function App() {
     fetchIPInfo();
   }, []);
 
-  
+
   console.log(loc)
 
   return (
-   <>
-    <div className='p-4 bg-blue-400 text-center'>Test</div>
-    
-    {loc &&  
-     <Map loc={loc} />}
-   </>
+    <>
+      <div className='p-4 bg-green-400   text-cyan-950 text-center'>Test by batman</div>
+
+      {loc &&
+        <Map loc={loc} />}
+    </>
   )
 }
 
